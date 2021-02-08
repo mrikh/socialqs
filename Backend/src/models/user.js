19d13e0f63890e3fa150734d0ab950dc2b25294c
Mayank Rikh <19018797@studentmail.ul.ie>
2021-02-08 19:23:26 +0000

    Updated login API for social handling

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -49,6 +49,9 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+    socialId : {
+        type : String
+    },
     token : {
         type : String,
         required : true
--- Backend/src/routers/user.js
+++ Backend/src/routers/user.js
@@ -4,7 +4,6 @@ const constants = require('../utils/constants')
 const {sendVerificationMail, sendForgotMail} = require('../emails/account')
 const auth = require('../middleware/auth')
 const bcrypt = require('bcrypt')
-const speakeasy = require('speakeasy')
 const otpHandler = require('../utils/otp')
 
 const router = new express.Router()
@@ -16,6 +15,55 @@ router.post('/users/signUp', async (req, res, next) => {
         sendVerificationMail(user)
         res.send({code : 200, message : constants.success_signup, data : {user, token}})
     }catch (error){
+        next(error)
+    }
+})
+
+router.post('/users/login', async (req, res, next) => {
+    try{
+        const params = req.body
+
+        if (!params.email || (!params.password && !params.socialId)){
+            const error = new Error(constants.params_missing)
+            error.statusCode = 422
+            throw error
+        }
+
+        const user = await User.findOne({email : params.email})
+
+        if (!user){
+            const error = new Error(constants.user_not_found)
+            error.statusCode = 404
+            throw error
+        }
+
+        if (params.password){
+            const isMatch = await bcrypt.compare(params.password, user.password)
+            if (isMatch){
+                const token = await user.generateAuthToken()
+                return res.send({code : 200, message : constants.success, data : {user, token}})
+            }else{
+                const error = new Error(constants.params_missing)
+                error.statusCode = 404
+                throw error
+            }
+        }else if (params.socialId){
+            //social id exists, different social id, social id wrong
+            if (user.socialId === params.socialId){
+                const token = await user.generateAuthToken()
+                return res.send({code : 200, message : constants.success, data : {user, token}})
+            }else{
+                const error = new Error(constants.params_missing)
+                error.statusCode = 404
+                throw error
+            }
+        }else{
+            const error = new Error(constants.params_missing)
+            error.statusCode = 404
+            throw error
+        }
+
+    }catch(error){
         next(error)
     }
 })
@@ -37,13 +85,10 @@ router.post('/users/resendVerification', auth, async (req, res, next) => {
 router.post('/users/verifyEmail', auth, async (req, res, next) => {
     try{
         const user = req.user
-
         if (!user){
             res.send({code : 404, message : constants.user_not_found})
         }else{
-    
             const tokenValidates = otpHandler.verifyOtp(user, req.body.otp)
-
             try{
                 if (tokenValidates){
                     user.emailVerified = true
@@ -61,40 +106,6 @@ router.post('/users/verifyEmail', auth, async (req, res, next) => {
     }
 })
 
-router.post('/users/login', async (req, res, next) => {
-    try{
-        const params = req.body
-        
-        if (!params.email || !params.password){
-            const error = new Error(constants.params_missing)
-            error.statusCode = 422
-            throw error
-        }
-        
-        const user = await User.findOne({email : params.email})
-        
-        if (!user){
-            const error = new Error(constants.user_not_found)
-            error.statusCode = 404
-            throw error
-        }
-
-        const isMatch = await bcrypt.compare(params.password, user.password)
-
-        if (isMatch){
-            const token = await user.generateAuthToken()
-            return res.send({code : 200, message : constants.success, data : {user, token}})
-        }else{
-            const error = new Error(constants.params_missing)
-            error.statusCode = 404
-            throw error
-        }
-
-    }catch(error){
-        next(error)
-    }
-})
-
 router.post('/users/forgotPass', async (req, res, next) => {
 
     try{
@@ -106,40 +117,9 @@ router.post('/users/forgotPass', async (req, res, next) => {
             error.statusCode = 404
             throw error
         }
-
         sendForgotMail(email)
         res.send({code : 200, message : constants.forgot_success})
     }catch (error){
-        next(error)
-    }
-})
-
-router.post('/users/sendOTP', auth, async (req, res, next) => {
-
-    const token = otpHandler.generateOtp(req.user)
-    const phoneNumber = req.body.phoneNumber
-
-    try{
-        res.send({code : 200, message : constants.success, data: {otp : token}})  
-    }catch(error){
-        next(error)
-    }
-})
-
-router.post('/users/verifyOTP', auth, async (req, res, next) => {
-    
-    const tokenValidates = otpHandler.verifyOtp(req.user, req.body.token)
-    const user = req.user
-    
-    try{
-        if (tokenValidates){
-            user.phoneVerified = true
-            await user.save()
-            res.send({code : 200, message: constants.verification_success, data : {user}})
-        }else{
-            res.send({code : 404, message : constants.verification_failed})
-        }
-    }catch(error){
         next(error)
     }
 })
