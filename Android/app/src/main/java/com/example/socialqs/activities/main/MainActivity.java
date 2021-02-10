f37b851f95f07b7187b8ce5a013485a8f6562bc2
Sinead-3 <60734340+Sinead-3@users.noreply.github.com>
2021-02-10 22:19:11 +0000

    Video Post Fragment (First Pass)

--- Android/app/src/main/java/com/example/socialqs/activities/main/MainActivity.java
+++ Android/app/src/main/java/com/example/socialqs/activities/main/MainActivity.java
@@ -11,27 +11,30 @@ import com.androidnetworking.AndroidNetworking;
 import com.example.socialqs.R;
 import com.example.socialqs.activities.prelogin.PreLoginActivity;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        AndroidNetworking.initialize(getApplicationContext());
-
-        Intent myIntent;
+        setContentView(R.layout.activity_home);
+//        setContentView(R.layout.activity_main);
+//
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//        AndroidNetworking.initialize(getApplicationContext());
+//
+//        Intent myIntent;
 //        try{
 //            JSONObject object = Utilities.getInstance().fetchJsonObject(getApplicationContext(), "user");
 //            UserModel current = new UserModel(object.getInt("id"), object.getString("name"), object.getString("email"));
 //            UserModel.currentUser = current;
 //            myIntent = new Intent(MainActivity.this, LandingSearchActivity.class);
 //        }catch (Exception e){
-            //no user model so go to login
-        myIntent = new Intent(MainActivity.this, PreLoginActivity.class);
+//            no user model so go to login
+//        myIntent = new Intent(MainActivity.this, PreLoginActivity.class);
 //        }
-        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(myIntent);
+//        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(myIntent);
     }
 }
