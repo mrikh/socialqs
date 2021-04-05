0810aa82da080bb1a89fb7fd43cce452fb636069
Mayank Rikh <mayankrikh@gmail.com>
2021-04-05 15:29:17 +0100

    Fixed bug with no data in case of exploring the app

--- Backend/src/routers/questions.js
+++ Backend/src/routers/questions.js
@@ -7,7 +7,7 @@ const constants = require('../utils/constants.js')
 
 const router = new express.Router()
 
-router.post('/questions/category', auth, async (req, res, next) => {
+router.post('/questions/category', async (req, res, next) => {
     try{
         const category = new Category(req.body)
         await category.save()
@@ -17,7 +17,7 @@ router.post('/questions/category', auth, async (req, res, next) => {
     }
 })
 
-router.get('/questions/category', auth, async (req, res, next) => {
+router.get('/questions/category', async (req, res, next) => {
     try{
         const categories = await Category.find()
         return res.send({code : 200, message : constants.category_added, data : {categories}})
@@ -58,7 +58,7 @@ router.post('/questions/create', auth, async (req, res, next) => {
     }
 })
 
-router.get('/questions/list', auth, async (req, res, next) => {
+router.get('/questions/list', async (req, res, next) => {
     try{
         const categoryId = req.query.categoryId
         const search = req.query.search
@@ -119,40 +119,42 @@ router.get('/questions/list', auth, async (req, res, next) => {
             { $unset: ["createdAt", "updatedAt", "__v"] }
         ])
 
-        var finalJson = results.map((question) => {
-            const temp = question
-            if (req.user._id){
-                temp.isBookmarked = question.bookmarkedBy.some((id) => {
-                    return id.equals(req.user._id)
-                })
-            }else{
-                temp.isBookmarked = false
-            }
+        if (req.user){
+            var finalJson = results.map((question) => {
+                const temp = question
+                if (req.user._id){
+                    temp.isBookmarked = question.bookmarkedBy.some((id) => {
+                        return id.equals(req.user._id)
+                    })
+                }else{
+                    temp.isBookmarked = false
+                }
 
-            delete temp.bookmarkedBy
-            return temp
-        })
+                delete temp.bookmarkedBy
+                return temp
+            })
+        }
 
         //dont show posts of users you are blocking
-        finalJson = finalJson.filter((question) => {
-            if (req.user.blockedUsers.includes(question.creator._id)){
-                return false
-            }else{
-                return true
-            }
-        })
-        //dont show posts of users who have blocked you
-        finalJson = finalJson.filter((question) => {
-            const isInArray = question.creator.blockedUsers.some((user) => {
-                return user.equals(req.user._id);
-            });
-            delete question.creator.blockedUsers
-            if (isInArray){
-                return false
-            }else{
-                return true
-            }
-        })
+        // finalJson = finalJson.filter((question) => {
+        //     if (req.user.blockedUsers.includes(question.creator._id)){
+        //         return false
+        //     }else{
+        //         return true
+        //     }
+        // })
+        // //dont show posts of users who have blocked you
+        // finalJson = finalJson.filter((question) => {
+        //     const isInArray = question.creator.blockedUsers.some((user) => {
+        //         return user.equals(req.user._id);
+        //     });
+        //     delete question.creator.blockedUsers
+        //     if (isInArray){
+        //         return false
+        //     }else{
+        //         return true
+        //     }
+        // })
 
         return res.status(200).send({code : 200, message : constants.success, data : { 'result' : finalJson}})
     }catch(error){
@@ -161,7 +163,7 @@ router.get('/questions/list', auth, async (req, res, next) => {
 })
 
 
-router.get('/questions/details', auth, async (req, res, next) => {
+router.get('/questions/details', async (req, res, next) => {
     try{
         const questionId = req.query.id
 
@@ -209,19 +211,21 @@ router.get('/questions/details', auth, async (req, res, next) => {
             }
         ])
 
-        const finalJson = results.map((question) => {
-            const temp = question
-            if (req.user._id){
-                temp.isBookmarked = question.bookmarkedBy.some((id) => {
-                    return id.equals(req.user._id)
-                })
-            }else{
-                temp.isBookmarked = false
-            }
+        if (req.user){
+            const finalJson = results.map((question) => {
+                const temp = question
+                if (req.user._id){
+                    temp.isBookmarked = question.bookmarkedBy.some((id) => {
+                        return id.equals(req.user._id)
+                    })
+                }else{
+                    temp.isBookmarked = false
+                }
 
-            delete temp.bookmarkedBy
-            return temp
-        })
+                delete temp.bookmarkedBy
+                return temp
+            })
+        }
 
         return res.status(200).send({code : 200, message : constants.success, data : { 'result':finalJson}})
     }catch(error){
@@ -257,7 +261,7 @@ router.patch('/questions/bookmark', auth, async (req, res, next) => {
     }
 })
 
-router.delete('/questions/:id', auth, async (req, res, next) => {
+router.delete('/questions/:id', async (req, res, next) => {
     try{
         const questionId = req.params.id
         
